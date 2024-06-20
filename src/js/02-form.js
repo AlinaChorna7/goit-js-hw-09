ceddf20d18f6c17d46b27ff706964156b6290fb2
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-06-20 19:59:10 +0200

    mb

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,38 +1,47 @@
-let formData ={
-email: "" ,
-message: "" 
-};
-
+let formData = {
+    email: "",
+    message: ""
+}
 
 const emailInput = document.getElementById('emailInput');
 const messageInput = document.getElementById('messageInput');
-const form = document.querySelector('.feedback-form')
+const form = document.querySelector('.feedback-form');
 
 function saveFormData(){
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-};
+
+}
 
 function loadFormData(){
     const storedData = localStorage.getItem('feedback-form-state');
-    if (storedData){
-        formData = JSON.parse(storedData);
-        emailInput.value = formData.email;
-        messageInput.value = formData.message;
+    if(storedData){
+        formData = JSON.parse('storedData');
+        emailInput = formData.email;
+        messageInput = formData.message;
+
     }
 }
 
-form.addEventListener('submit', function(event) {
+window.addEventListener('load', loadFormData);
+
+form.addEventListener('input', function(event){
+    const { name , value } = event.target;
+    formData[name]= value.trim();
+    saveFormData();
+})
+
+form.addEventListener('submit', function(event){
 event.preventDefault();
-if (formData.email.trim() === '' || formData.message.trim() === ''){
-    alert ('Fill please all fields');
-} else{
-    console.log(formData); 
-    localStorage.removeItem('feedback-form-state')
-formData = {email: "" , message: ""}
-emailInput.value = '';
-messageInput.value = '';
+const emailValue = formData.email.trim();
+const messageValue = formData.message.trim();
 
+if(!emailValue || !messageValue){
+    return alert('Fill please all fields');
 }
+
+console.log(formData);
+localStorage.removeItem('feedback-form-state');
+formData = { email: "", message: "" };
+      emailInput.value = '';
+      messageInput.value = '';
 });
-
-
